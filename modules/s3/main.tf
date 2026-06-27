509db0f01609ab4f344aa41aa26390c483ddedc7
Jay <jairamsai248@gmail.com>
2026-06-27 01:39:23 -0400

    modifed nsg

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -33,11 +33,11 @@ resource "azurerm_storage_account" "exam1" {
 #  } 
 # }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "example-security-group"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
+# resource "azurerm_network_security_group" "example" {
+#   name                = "example-security-group"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+# }
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
