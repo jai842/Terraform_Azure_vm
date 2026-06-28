c9637f4a17b982913fcb21fe0e74add2a05c4f1e
Jay <jairamsai248@gmail.com>
2026-06-28 01:17:47 -0400

    modified

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -41,8 +41,8 @@ resource "azurerm_storage_account" "exam1" {
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
   
 
