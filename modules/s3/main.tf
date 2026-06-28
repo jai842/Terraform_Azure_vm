3b38934c712569db1ad57abd9ea79be47a27541f
Jay <jairamsai248@gmail.com>
2026-06-28 06:10:24 -0400

    done4

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "exam1" {
   name = var.storage_account_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
   location = var.location
   account_tier = "Standard"
   account_replication_type = "LRS"
