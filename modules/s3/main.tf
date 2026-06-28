899a1de9ccee46622e9f167e673f4ede613bf8c9
Jay <jairamsai248@gmail.com>
2026-06-28 05:54:54 -0400

    done2

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "var.resource_group_name"
-  location = "var.location"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 # resource "azurerm_resource_group" "rga" {
